1477b5b25081ea4117bdfb83cb02edeab5e749db
willy <willybalt.2017@gmail.com>
2025-06-16 20:41:04 -0400

    se agrego controlador producto

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Producto;
 use Illuminate\Http\Request;
 
 class ProductoController extends Controller
@@ -9,9 +10,11 @@ class ProductoController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $search = $request->search;
+        $limit = $request->limit;
+        return Producto::where("nombre","LIKE","%$search%")->paginate($limit);
     }
 
     /**
@@ -19,7 +22,28 @@ class ProductoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            "nombre" => "required",
+            "unidad_medida" => "required",
+            "categoria_id" => "required",
+            "fecha_registro" => "required"
+        ]);
+
+        $prod = new Producto();
+        $prod->nombre = $request->nombre;
+        $prod->unidad_medida = $request->unidad_medida;
+        $prod->categoria_id = $request->categoria_id;
+        $prod->fecha_registro = $request->fecha_registro;
+        $prod->descripcion = $request->descripcion;
+        $prod->codigo_barra = $request->codigo_barra;
+        $prod->marca = $request->marca;
+        $prod->precio_venta_actual = $request->precio_venta_actual;
+        $prod->stock_minimo = $request->stock_minimo;
+        $prod->activo = $request->activo;
+        
+        $prod->save();
+
+        return response()->json($prod);
     }
 
     /**
