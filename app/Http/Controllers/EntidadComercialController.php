bf5148072274e3582ed5ad89f82466a3c23112a7
willy <willybalt.2017@gmail.com>
2025-06-28 21:41:44 -0400

    se agrego el crud entidad-comercial

--- app/Http/Controllers/EntidadComercialController.php
+++ app/Http/Controllers/EntidadComercialController.php
@@ -2,16 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contacto;
+use App\Models\EntidadComercial;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class EntidadComercialController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $query = $request->search;
+
+        if (isset($query)) {
+            $entidadComercial = EntidadComercial::orderBy('id', 'desc')->where('razon_social', "LIKE", $query)
+                ->with(["contactos"])
+                ->paginate(20);
+        } else {
+            $entidadComercial = EntidadComercial::orderBy('id', 'desc')->with(["contactos"])
+                ->paginate(20);
+        }
+
+        return response()->json($entidadComercial, 200);
     }
 
     /**
@@ -19,7 +33,44 @@ class EntidadComercialController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            "tipo" => "required"
+        ]);
+
+        DB::beginTransaction();
+
+        try {
+            //code...
+            $ec = new EntidadComercial();
+            $ec->tipo = $request->tipo;
+            $ec->razon_social = $request->razon_social;
+            $ec->ci_nit_ruc_rut = $request->ci_nit_ruc_rut;
+            $ec->telefono = $request->telefono;
+            $ec->direccion = $request->direccion;
+            $ec->correo = $request->correo;
+
+            $ec->save();
+
+            if (isset($request->nombre_completo)) {
+                $contacto = new Contacto();
+                $contacto->nombre_completo = $request->nombre_completo;
+                $contacto->rol_contacto = $request->rol_contacto;
+                $contacto->telefono_secundario = $request->telefono_secundario;
+                $contacto->correo_secundario = $request->correo_secundario;
+                $contacto->observaciones = $request->observaciones;
+                $contacto->entidad_comercial_id = $ec->id;
+                $contacto->save();
+            }
+
+            DB::commit();
+
+            return response()->json(["message" => "Entidad Comercial registrada", 200]);
+        } catch (\Exception $e) {
+            //throw $th;
+            DB::rollBack();
+
+            return response()->json(["message" => "Error al registrar Entidad Comercial", 400, "error" => $e->getMessage()]);
+        }
     }
 
     /**
@@ -27,7 +78,7 @@ class EntidadComercialController extends Controller
      */
     public function show(string $id)
     {
-        //
+        return response()->json(EntidadComercial::with(['contactos'])->find($id), 200);
     }
 
     /**
@@ -35,7 +86,32 @@ class EntidadComercialController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            "tipo" => "required"
+        ]);
+
+        DB::beginTransaction();
+
+        try {
+            //code...
+            $ec = EntidadComercial::find($id);
+            $ec->tipo = $request->tipo;
+            $ec->razon_social = $request->razon_social;
+            $ec->ci_nit_ruc_rut = $request->ci_nit_ruc_rut;
+            $ec->telefono = $request->telefono;
+            $ec->direccion = $request->direccion;
+            $ec->correo = $request->correo;
+            $ec->update();
+
+            DB::commit();
+
+            return response()->json(["message" => "Entidad Comercial registrada", 200]);
+        } catch (\Exception $e) {
+            //throw $th;
+            DB::rollBack();
+
+            return response()->json(["message" => "Error al registrar Entidad Comercial", 400, "error" => $e->getMessage()]);
+        }
     }
 
     /**
@@ -43,6 +119,11 @@ class EntidadComercialController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $ec = EntidadComercial::find($id);
+        $ec->activo = false;
+
+        $ec->update();
+
+        return response()->json(["message" => "Entidad Comercial inactiva", 400]);
     }
 }
